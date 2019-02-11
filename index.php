0c7e1b3c6a229a3d1b9652fbba64a44f2a5c1aff
twentyfortysix <2046@2046.cz>
2019-02-11 16:43:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ if( ICL_LANGUAGE_CODE == 'en' ){
 }else{
 	$data['top_menu'] = new TimberMenu('top_menu');
 }
-$data['languages'] = icl_get_languages('skip_missing=0');
+$data['languages'] = apply_filters( 'wpml_active_languages', NULL, 'orderby=code&order=asc&skip_missing=0' );
 $data['current_language'] = ICL_LANGUAGE_NAME;
 
 if(is_front_page()){
