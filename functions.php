e548df773dc9caa59e16035920e9c2097ab7a5cb
twentyfortysix <2046@2046.cz>
2020-02-11 19:01:30 +0100

    Update functions.php
    added dashboard removal logic

--- functions.php
+++ functions.php
@@ -139,5 +139,25 @@ function my_custom_fonts() {
     }
   </style>';
 }
+// NO dashbord nonsense
+function dashboard_redirect(){
+    wp_redirect(admin_url('edit.php?post_type=page'));
+}
+add_action('load-index.php','dashboard_redirect');
 
+function login_redirect( $redirect_to, $request, $user ){
+    return admin_url('edit.php?post_type=page');
+}
+add_filter('login_redirect','login_redirect',10,3);
 
+function remove_menus () {
+    global $menu;
+    $restricted = array(__('Dashboard'));
+    //$restricted = array(__('Dashboard'), __('Posts'), __('Media'), __('Links'), __('Pages'), __('Appearance'), __('Tools'), __('Users'), __('Settings'), __('Comments'), __('Plugins'));
+    end($menu);
+    while(prev($menu)){
+        $value = explode(' ',$menu[key($menu)][0]);
+        if(in_array($value[0]!= NULL?$value[0]:'',$restricted)){unset($menu[key($menu)]);}
+    }
+}
+add_action('admin_menu','remove_menus');
