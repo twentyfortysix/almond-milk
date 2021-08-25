b234d66f8d68eefdf2ae9e9bc53acef8bb563442
twentyfortysix <2046@2046.cz>
2021-08-25 19:00:39 +0200

    init base/frontpage data

--- index.php
+++ index.php
@@ -4,9 +4,10 @@ $data = Timber::get_context();
 $data['top_menu'] = new TimberMenu('top_menu_'.ICL_LANGUAGE_CODE);
 $data['languages'] = apply_filters( 'wpml_active_languages', NULL, 'orderby=code&order=asc&skip_missing=0' );
 $data['current_language'] = ICL_LANGUAGE_CODE;
+$data['base'] = new TimberPost(get_option('page_on_front'));
 
 if(is_front_page()){
-	$data['post'] = new TimberPost();
+	$data['post'] = $data['base']
 	Timber::render('frontpage.twig', $data);
 }elseif(is_archive()){
 	$data['posts'] = Timber::get_posts();
