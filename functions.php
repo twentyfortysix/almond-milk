9a114118da6d03d4ec2389de3751c95437972b34
twentyfortysix <2046@2046.cz>
2022-05-14 13:33:57 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ require_once(TEMPLATEPATH. '/include/remove-page-wyswig.php');
 // require_once(TEMPLATEPATH. '/include/p2p-registers.php');
 // require_once(TEMPLATEPATH. '/include/remove-taxonomies.php');
 // require_once(TEMPLATEPATH. '/include/page-step-navigation.php');
-// require_once(TEMPLATEPATH. '/include/create-taxonomies.php');
+// require_once(TEMPLATEPATH. '/include/register-taxonomies.php');
 // require_once(TEMPLATEPATH. '/include/googleAPI.php');
 // require_once(TEMPLATEPATH. '/include/GQL_ACF.php');
 
