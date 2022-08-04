f0fae513396e0c03fb7388ce55668b7c05fc20b1
twentyfortysix <2046@2046.cz>
2022-08-04 11:08:38 +0200

    nbsp filter update

--- include/timber.php
+++ include/timber.php
@@ -36,25 +36,26 @@ add_filter( 'timber/twig/filters', function( $filters ) {
 
 
 function fix_typo( $text ) {
-    // remove multiple spaces
-    $text =  preg_replace('/[\s]+/mu', ' ', $text);
-    
-	$strings = [
-		// ve spojení neslabičných předložek k, s, v, z s následujícím slovem, např. k mostu, s bratrem, v Plzni, z nádraží,
-		'k', 's', 'v', 'z',
-		// ve spojení slabičných předložek o, u a spojek a, i s výrazem, který po nich následuje, např. u babičky, o páté,
-		'o', 'u', 'a',
-		// mezi číslem a zkratkou počítaného předmětu nebo písmennou značkou jednotek a měn, např. 5 str., 8 hod., s. 53, č. 9, obr. 1, tab. 3, ..
-		'např.', 'str.', 's.', 'č.', 'obr.', 'tab',
-		// mezi zkratkami typu tj., tzv., tzn. a výrazem, který za nimi bezprostředně následuje, např. tzv. klikání,
-		'tj.', 'tzv.', 'tzn.',
-		// mezi zkratkou titulu nebo hodnosti uváděnou před osobním jménem, např. p. Čečetková, mjr. Veselý, Ing. Poliaková 
-		'p.', 'MgA', 'Ing', 'mjr.', 'A.',  'art.', 'doc.', 'Ph.'
-		];
+
+    $strings = [
+        // ve spojení neslabičných předložek k, s, v, z s následujícím slovem, např. k mostu, s bratrem, v Plzni, z nádraží,
+        'k', 's', 'v', 'z',
+        // ve spojení slabičných předložek o, u a spojek a, i s výrazem, který po nich následuje, např. u babičky, o páté,
+        'o', 'u', 'a',
+        // mezi číslem a zkratkou počítaného předmětu nebo písmennou značkou jednotek a měn, např. 5 str., 8 hod., s. 53, č. 9, obr. 1, tab. 3, ..
+        'např.', 'str.', 's.', 'č.', 'obr.', 'tab',
+        // mezi zkratkami typu tj., tzv., tzn. a výrazem, který za nimi bezprostředně následuje, např. tzv. klikání,
+        'tj.', 'tzv.', 'tzn.',
+        // mezi zkratkou titulu nebo hodnosti uváděnou před osobním jménem, např. p. Čečetková, mjr. Veselý, Ing. Poliaková 
+        'p.', 'Mga.', 'MgA.', 'Ing.', 'mjr.', 'A.',  'art.', 'doc.', 'Ph.', 'Mgr.', 'arch.'
+        ];
 
     for ($i=0; $i < count($strings); $i++) { 
-    	// $(this).html(text.replace(new RegExp(' '+myArrayItem+' ', 'g') , ' '+myArrayItem+'&nbsp;'));
-    	$text = str_replace( ' ' . $strings[$i] . ' ', ' ' . $strings[$i] . '&nbsp;', $text);
+        // replace multiple cases in one go
+        $text = str_replace( [' ' . $strings[$i] . ' ', "\n" . $strings[$i] . ' ', ">" . $strings[$i] . ' '],
+                             [' ' . $strings[$i] . '&nbsp;', "\n" . $strings[$i] . '&nbsp;', ">" . $strings[$i] . '&nbsp;'],
+                             $text
+                );
     }
     
     return $text;
