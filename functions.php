ecc6797df78de29dd417e07d817d4771fc5e869b
twentyfortysix <2046@2046.cz>
2023-04-03 19:45:58 +0200

    deleted parts which are part of include/wp_cleanup

--- functions.php
+++ functions.php
@@ -52,66 +52,6 @@ function f_2046_add_scripts() {
 add_action('wp_enqueue_scripts', 'f_2046_add_scripts');
 
 
-
-// remove image dimensions
-add_filter( 'post_thumbnail_html', 'remove_thumbnail_dimensions', 10 );
-
-// Removes attached image sizes as well
-add_filter( 'wp_calculate_image_srcset_meta', '__return_empty_array' );
-
-// fuck off emoji
-remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-remove_action( 'wp_print_styles', 'print_emoji_styles' );
-remove_action( 'admin_print_styles', 'print_emoji_styles' );
-
-
-
-$role_object = get_role( 'editor' );
-$role_object->add_cap( 'edit_theme_options' );
-function hide_menu() {
-
-    if (current_user_can('editor')) {
-
-        remove_submenu_page( 'themes.php', 'themes.php' ); // hide the theme selection submenu
-        remove_submenu_page( 'themes.php', 'widgets.php' ); // hide the widgets submenu
-        remove_submenu_page( 'themes.php', 'customize.php?return=%2Fwp-admin%2Ftools.php' ); // hide the customizer submenu
-        remove_submenu_page( 'themes.php', 'customize.php?return=%2Fwp-admin%2Ftools.php&#038;autofocus%5Bcontrol%5D=background_image' ); // hide the background submenu
-        remove_submenu_page( 'themes.php', 'customize.php?return=%2Fcontent%2Fwp-admin%2Fprofile.php' ); // hide the background submenu
-
-
-        // these are theme-specific. Can have other names or simply not exist in your current theme.
-        // remove_submenu_page( 'themes.php', 'yiw_panel' );
-        remove_submenu_page( 'themes.php', 'custom-header' );
-        remove_submenu_page( 'themes.php', 'custom-background' );
-    }
-}
-
-add_action('admin_head', 'hide_menu');
-
-// NO dashbord nonsense
-function dashboard_redirect(){
-    wp_redirect(admin_url('edit.php?post_type=page'));
-}
-add_action('load-index.php','dashboard_redirect');
-
-function login_redirect( $redirect_to, $request, $user ){
-    return admin_url('edit.php?post_type=page');
-}
-add_filter('login_redirect','login_redirect',10,3);
-
-function remove_menus () {
-    global $menu;
-    $restricted = array(__('Dashboard'));
-    //$restricted = array(__('Dashboard'), __('Posts'), __('Media'), __('Links'), __('Pages'), __('Appearance'), __('Tools'), __('Users'), __('Settings'), __('Comments'), __('Plugins'));
-    end($menu);
-    while(prev($menu)){
-        $value = explode(' ',$menu[key($menu)][0]);
-        if(in_array($value[0]!= NULL?$value[0]:'',$restricted)){unset($menu[key($menu)]);}
-    }
-}
-add_action('admin_menu','remove_menus');
-
 function encode_email($e) {
     $output = '';
     for ($i = 0; $i < strlen($e); $i++) { $output .= '&#'.ord($e[$i]).';'; }
