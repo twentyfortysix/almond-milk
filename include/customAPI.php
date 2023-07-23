6352f1e422e213073ea0eded90858ef2b1a16822
twentyfortysix <2046@2046.cz>
2023-07-23 20:54:03 +0200

    Update customAPI.php
    little validation fix

--- include/customAPI.php
+++ include/customAPI.php
@@ -1,33 +1,84 @@
 <?php
 add_action( 'rest_api_init', function () {
     // album path
-  register_rest_route( '2046/v1', '/album/(?P<id>\d+)', array(
+    register_rest_route( '2046/v1', '/addon/(?P<id>\d+)', 
+    [
     'methods' => 'GET',
-    'callback' => 'get_album_json',
-    'args' => array(
-      'id' => array(
-        'validate_callback' => function($param, $request, $key) {
-          return is_numeric( $param );
-        }
-      ),
-    ),
-  ) );
-    register_rest_route( '2046/v1', '/concert/lang=(?P<lang>[a-zA-Z]+)&id=(?P<id>\d+)',
-        array(
-            'methods' => WP_REST_Server::READABLE,
-            'callback' => 'getConcert',
-            'args' => array(
-                'lang' => array(
-                    'validate_callback' => function($param, $request, $key) {
-                        return sanitize_text_field( $param );
-                    }
-                ),
-                'id' => array(
-                    'validate_callback' => function($param, $request, $key) {
-                        return is_numeric( $param );
-                    }
-                ),
-            )
-        )
+    'callback' => 'get_addon_json',
+        'args' => [
+          'id' => [
+            'sanitize_callback' => 'absint',
+          ],
+        ],
+    ]
+    );
+    register_rest_route(
+        '2046/v1',
+        '/addons',
+        [
+            'methods' => 'GET',
+            'callback' => 'get_addons_json',
+            'args' => [
+                'order' => [
+                    'sanitize_callback' => 'sanitize_title',
+                    // 'validate_callback' => truthy function_that_validates() 
+                ],
+                'orderby' => [
+                    'sanitize_callback' => 'sanitize_title',
+                ],
+                'pricing' => [
+                    'sanitize_callback' => 'sanitize_title',
+                ],
+                'type' => [
+                    'sanitize_callback' => 'absint',
+                ],
+                'author' => [
+                    'sanitize_callback' => 'absint',
+                ],
+                'offset' => [
+                    'sanitize_callback' => 'absint',
+                ],
+                's' => [
+                    'sanitize_callback' => 'wp_kses_post',
+                ]
+            ]
+        ]
     );
 });
+
+
+
+function get_addons_json($request){
+    $data = [];
+    // $args = [
+    //     'post_type' => 'post',
+    //     'post_status' => 'publish',
+    //     'posts_per_page' => '10',
+    //     'paged' => isset($request['offset']) ? $request['offset'] : 1,
+    //     'orderby' => 'date',
+    //     'order' => 'DESC',
+    // ];
+    // // search query
+    // if(isset($request['s'])){
+    //     $data['s'] = $request['s'];
+    // }
+    // // page
+    // if(isset($request['offset'])){
+    //     $data['paged'] = $request[''];
+    // }
+    // $data['items'] = Timber::get_posts($args);
+    $data[] = $request->get_param('offset');
+    $data[] = $request->get_param('s');
+    $resp = new WP_REST_Response($data);
+    return $resp;
+    wp_die();
+}
+
+function get_addon_json($request ){
+    $data = [];
+    $context = Timber::context();
+    $data['item'] = Timber::get_post(intval($request['id']));
+    $resp = new WP_REST_Response($data);
+    return $resp;
+    wp_die();
+}
