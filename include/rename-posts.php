2727a043a4233cdb84fdb438267f8390befd0187
twentyfortysix <2046@2046.cz>
2024-02-19 15:52:45 +0100

    to czech

--- include/rename-posts.php
+++ include/rename-posts.php
@@ -2,8 +2,8 @@
 function hook_change_post_label() {
     global $menu;
     global $submenu;
-    $menu[5][0] = 'Partials'; // 5 = post
-    $submenu['edit.php'][5][0] = 'Partials';
+    $menu[5][0] = 'Akce'; 
+    $submenu['edit.php'][5][0] = 'Akce';
 }
 add_action( 'admin_menu', 'hook_change_post_label' );
 
@@ -11,15 +11,15 @@ add_action( 'admin_menu', 'hook_change_post_label' );
 function hook_change_post_object() {
     global $wp_post_types;
     $labels = &$wp_post_types['post']->labels;
-    $labels->name = 'Partial';
-    $labels->singular_name = 'Partial';
-    $labels->add_new = 'Add Partial';
-    $labels->add_new_item = 'Add Partial';
-    $labels->edit_item = 'Edit Partial';
-    $labels->new_item = 'Partial';
-    $labels->view_item = 'View Partial';
-    $labels->search_items = 'Search Partial';
-    $labels->not_found = 'No Partial found';
-    $labels->not_found_in_trash = 'No Partial found in Trash';
+    $labels->name = 'Akce';
+    $labels->singular_name = 'Akce';
+    $labels->add_new = 'Přidat akci';
+    $labels->add_new_item = 'Přidat akci';
+    $labels->edit_item = 'Editovat akci';
+    $labels->new_item = 'Nová akce';
+    $labels->view_item = 'Zobrazit akci';
+    $labels->search_items = 'Vyhledat akci';
+    $labels->not_found = 'Nenalezeno';
+    $labels->not_found_in_trash = 'V koši není';
 }
 add_action( 'init', 'hook_change_post_object' );
